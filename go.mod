059794e5f362bfd57ee8ad85826700053862df1d
2asm <0077dk0077@gmail.com>
2023-12-12 11:21:40 +0530

    added bloom-filter

--- go.mod
+++ go.mod
@@ -3,3 +3,8 @@ module github.com/2asm/go_crawler
 go 1.20
 
 require golang.org/x/net v0.19.0
+
+require (
+	github.com/2asm/bloom-filter v0.0.0-20231212054015-6c52262c2b07 // indirect
+	github.com/spaolacci/murmur3 v1.1.0 // indirect
+)
--- main.go
+++ main.go
@@ -8,12 +8,14 @@ import (
 	"strings"
 	"sync"
 
+	"github.com/2asm/bloom-filter/bloomfilter"
 	"golang.org/x/net/html"
 )
 
 type Crawler struct {
 	mu    sync.Mutex
 	vis   map[string]bool
+	bf    *bloomfilter.BloomFilter
 	seed  *url.URL
 	same  bool
 	sites chan *url.URL
@@ -26,14 +28,23 @@ func NewCrawler(seed *url.URL, same bool) *Crawler {
 		same:  same,
 		sites: make(chan *url.URL, 1),
 		vis:   make(map[string]bool),
+        bf: bloomfilter.NewBloomFilter(0.01,1000),
 	}
-	c.vis[seed.String()] = true
+	//c.vis[seed.String()] = true
+    c.bf.Add(seed.String())
 	c.sites <- seed
 	return c
 }
 
+func (c *Crawler) Crawl() {
+	for u := range c.sites {
+		log.Printf("%s\n", u.String())
+		go c.handle1(u)
+	}
+}
+
 func (c *Crawler) handle1(URL *url.URL) {
-    // blocking
+	// blocking
 	resp, err := http.Get(URL.String())
 	if err != nil {
 		log.Printf("--> Invalid URL: %s\n", URL)
@@ -47,23 +58,35 @@ func (c *Crawler) handle1(URL *url.URL) {
 			return
 		case token_type == html.StartTagToken:
 			token := z.Token()
-			if token.Data != "a" { continue }
+			if token.Data != "a" {
+				continue
+			}
 			for _, a := range token.Attr {
-				if a.Key != "href" { continue }
+				if a.Key != "href" {
+					continue
+				}
 				var u *url.URL
 				if strings.HasPrefix(a.Val, "https://") || strings.HasPrefix(a.Val, "http://") {
 					parsedUrl, err := url.Parse(a.Val)
-					if err != nil { break }
+					if err != nil {
+						break
+					}
 					u = parsedUrl
 				} else {
 					u = &url.URL{Scheme: URL.Scheme, Host: URL.Host, Path: a.Val}
 				}
 
-                // don't break, return or error between mutext Lock and Unlock
+				// don't break, return or error between mutext Lock and Unlock
 				c.mu.Lock()
 				if !c.same || (c.same && u.Host == c.seed.Host) {
+                    /*
 					if !c.vis[u.String()] {
 						c.vis[u.String()] = true
+						c.sites <- u
+					}
+                    */
+					if !c.bf.Contains(u.String()) {
+                        c.bf.Add(u.String())
 						c.sites <- u
 					}
 				}
@@ -71,13 +94,6 @@ func (c *Crawler) handle1(URL *url.URL) {
 				break
 			}
 		}
-	}
-}
-
-func (c *Crawler) Crawl() {
-	for u := range c.sites {
-		log.Printf("%s\n", u.String())
-		go c.handle1(u)
 	}
 }
 
@@ -94,6 +110,6 @@ func main() {
 		log.Fatalf("--> Invalid seed URL: %s\n", seed)
 	}
 	c := NewCrawler(seedUrl, same_host)
-    c.Crawl()
+	c.Crawl()
 
 }
